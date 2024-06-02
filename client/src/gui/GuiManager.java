bf5c6a2bb934ea136e38e4c563dd79d6e601e108
sidimekov <dimasidim8582@gmail.com>
2024-06-02 20:34:59 +0300

    feat: add auth form

--- client/src/gui/GuiManager.java
+++ client/src/gui/GuiManager.java
@@ -1,4 +1,23 @@
 package gui;
 
+import java.util.ResourceBundle;
+
 public class GuiManager {
+    private static GuiManager instance;
+    private static ResourceBundle enProperties = ResourceBundle.getBundle("../resources/en_EN.properties");
+
+    private GuiManager() {
+        instance = this;
+    }
+
+    public static GuiManager getInstance() {
+        if (instance == null) {
+            instance = new GuiManager();
+        }
+        return instance;
+    }
+
+    public void run() {
+
+    }
 }
---
+++ client/src/gui/forms/AuthForm.java
@@ -0,0 +1,13 @@
+package gui.forms;
+
+import javax.swing.*;
+
+public class AuthForm {
+    private JPanel authForm;
+    private JLabel authHelloLabel;
+    private JTextField authLoginField;
+    private JLabel authLoginLabel;
+    private JButton authSignInButton;
+    private JPasswordField authPasswordField;
+    private JPanel authRightPanel;
+}
